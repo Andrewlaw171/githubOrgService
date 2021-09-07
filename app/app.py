0eae5fb688cca39bd259c55fcbc9c657c7b05707
Andrew Law <u6673406@anu.edu.au>
2021-09-07 17:50:27 +1000

    initial commit

--- app/app.py
+++ app/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/highest_follow_count')
 def get_highest_follow_count():
-    return 'test'
+    return None
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,11 @@
 import unittest
+from app.app import get_highest_follow_count
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+
+class TestApi(unittest.TestCase):
+    def test_get_highest_follow_count(self):
         self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
